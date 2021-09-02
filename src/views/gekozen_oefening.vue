5a0be143c881ee679098859c7e1691a64dacf936
rhhaar <raimond@keicreations.nl>
2021-09-02 16:46:02 +0200

    new kei-sport

---
+++ src/views/gekozen_oefening.vue
@@ -0,0 +1,11 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "chosen_exercise",
+};
+</script>
+
+<style scoped></style>
---
+++ src/views/gekozen_workout.vue
@@ -0,0 +1,11 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "chosen_workout",
+};
+</script>
+
+<style scoped></style>
